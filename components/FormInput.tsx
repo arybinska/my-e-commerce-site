32a80f600e90f1a494b8869584fc3a289865a778
arybinska <ajrybinska@gmail.com>
2022-11-28 16:09:59 +0100

    Formularze - część 1

---
+++ components/FormInput.tsx
@@ -0,0 +1,18 @@
+export const FormInput = () => {
+  return 
+    // <>
+    //   <input
+    //     className="relative w-full rounded-bl-lg border-gray-200 p-2.5 text-sm placeholder-gray-400 focus:z-10"
+    //     type="text"
+    //     {...register("cardExpirationDate", {
+    //       required: true,
+    //       validate: props.validate,
+    //     })}
+    //     id="card-expiration-date"
+    //     placeholder="MM / YY"
+    //   />
+    //   <span role="alert" className="text-red-500 font-bold text-sm">
+    //     {formState.errors.cardExpirationDate?.message}
+    //   </span>
+    // </>
+};
