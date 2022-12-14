d20aa7a88b624891c5302b0e5205fe3c17a428da
arybinska <ajrybinska@gmail.com>
2022-12-14 21:50:04 +0100

    polska walidacja koszyka

--- utils.ts
+++ utils.ts
@@ -2,13 +2,33 @@ import { MDXRemoteSerializeResult } from "next-mdx-remote";
 
 export type MarkdownResult = MDXRemoteSerializeResult<Record<string, unknown>>;
 
-export const validateCardExpirationDate = (value: string) => {
+export const validateCardExpirationDate = (value: any) => {
+  if (!value || !value.includes("/")) {
+    return false;
+  }
   if (value.length !== 5) {
-    return `Wprowadź dane w formacie MM/YY`;
+    return false;
   }
   const [month, year] = value.split("/");
   if (Number(month) > 12) {
-    return `Wprowadź prawidłowy miesiąc`;
+    return false;
+  }
+  if (Number(year) < 22 || Number(year) > 33) {
+    return false;
+  }
+  return true;
+};
+
+export const validatePostalCode = (value: any) => {
+  if (!value || !value.includes("-")) {
+    return false;
+  }
+  const [firstPart, secondPart] = value.split("-");
+  if (Number(firstPart) > 99 || firstPart.length != 2) {
+    return false;
+  }
+  if (Number(secondPart) > 999 || secondPart.length != 3) {
+    return false;
   }
   return true;
 };
